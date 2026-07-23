3f71623260cbbc776e7407030dc77682d5f5281c
laurensZero <88361046+laurensZero@users.noreply.github.com>
2026-07-23 13:52:32 +0800

    feat: 添加用户名修改功能，支持在用户界面中编辑和更新用户名

--- src/config/manageEntries.js
+++ src/config/manageEntries.js
@@ -169,7 +169,7 @@ export function useManageEntries() {
       iconMode: 'svg', iconClass: 'share-icon',
       iconPaths: ['M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71', 'M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71'],
       path: '/manage/shares',
-      stats: [{ label: t('manage.statUsage'), value: t('manage.statShareManage') }, { label: t('manage.statDependency'), value: t('manage.statGithubToken') }]
+      stats: [{ label: t('manage.statUsage'), value: t('manage.statShareManage') }, { label: t('manage.statDependency'), value: 'Supabase' }]
     },
     {
       key: 'language', group: 'app', title: t('manage.language'), kicker: 'Language',
