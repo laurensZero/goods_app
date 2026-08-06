8e8b68a1fc7776768751c123991944e1e7b42f6a
laurensZero <88361046+laurensZero@users.noreply.github.com>
2026-08-06 16:38:40 +0800

    fix: 修复getservertime的CORS错误

--- supabase/functions/get-server-time/index.ts
+++ supabase/functions/get-server-time/index.ts
@@ -16,7 +16,7 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts"
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type, x-device-id",
 }
 
 function json(body: unknown): Response {
